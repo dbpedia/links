d0a07b4294ecda8c2fbb248dae489fb17fdd42cd
Dimitris Kontokostas <jimkont@gmail.com>
2016-04-25 14:02:55 +0300

    rdfunit checking

--- code/src/main/java/org/dbpedia/links/ValidateRepo.java
+++ code/src/main/java/org/dbpedia/links/ValidateRepo.java
@@ -1,8 +1,15 @@
 package org.dbpedia.links;
 
+import org.aksw.rdfunit.enums.TestCaseExecutionType;
+import org.aksw.rdfunit.io.reader.RdfModelReader;
 import org.aksw.rdfunit.io.reader.RdfReader;
 import org.aksw.rdfunit.io.reader.RdfReaderException;
 import org.aksw.rdfunit.io.reader.RdfStreamReader;
+import org.aksw.rdfunit.model.interfaces.TestSuite;
+import org.aksw.rdfunit.model.interfaces.results.TestExecution;
+import org.aksw.rdfunit.sources.SchemaSource;
+import org.aksw.rdfunit.tests.generators.ShaclTestGenerator;
+import org.aksw.rdfunit.validate.wrappers.RDFUnitStaticValidator;
 import org.apache.jena.rdf.model.Model;
 
 import java.io.File;
@@ -10,6 +17,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.Arrays.stream;
+import static org.aksw.rdfunit.io.reader.RdfReaderFactory.createResourceReader;
+import static org.aksw.rdfunit.sources.SchemaSourceFactory.createSchemaSourceSimple;
 
 
 /**
@@ -75,13 +84,40 @@ public class ValidateRepo {
 
 
     private static void checkMetadataFiles(List<File> filesList) {
+
+        TestSuite testSuite = createTestSuiteWithShacl("/shacl_metadata.ttl");
+
         filesList.stream().forEach(file -> {
             String fileName = file.getAbsolutePath();
             if (fileName.endsWith("metadata.ttl")) {
-                // TODO RDFUnit check
+                RdfReader reader = new RdfStreamReader(fileName);
+                try {
+                    Model model = reader.read();
+                    TestExecution te = RDFUnitStaticValidator.validate(TestCaseExecutionType.shaclFullTestCaseResult, model,  testSuite);
+
+                    te.getTestCaseResults().stream().forEach( result -> {
+                        throw new IllegalArgumentException("In file: " + fileName + " resource: "  + result.getTestCaseUri() + " failed with message: " + result.getMessage());
+                    });
+                } catch (RdfReaderException e) {
+                    throw new RuntimeException("Syntax error in file:" + fileName, e);
+
+                    //Syntax error reading file...
+                }
+
             }
 
         });
+    }
+
+    public static TestSuite createTestSuiteWithShacl(String schemaSource) {
+        RdfReader ontologyShaclReader = null;
+        try {
+            ontologyShaclReader = new RdfModelReader(createResourceReader(schemaSource).read());
+        } catch (RdfReaderException e) {
+            throw new IllegalArgumentException(e);
+        }
+        SchemaSource ontologyShaclSource = createSchemaSourceSimple("tests", "http://rdfunit.aksw.org", ontologyShaclReader);
+        return new TestSuite(new ShaclTestGenerator().generate(ontologyShaclSource));
     }
 
     private static List<File> getAllFilesInFolderOrFile (File input)
