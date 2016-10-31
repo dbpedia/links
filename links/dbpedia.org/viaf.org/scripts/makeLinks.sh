5b93cc145b54767200388d12001ded5c2a86b07b
Robert <mam09cqz@studserv.uni-leipzig.de>
2016-10-31 18:01:45 +0100

    Updated resource link

--- links/dbpedia.org/viaf.org/scripts/makeLinks.sh
+++ links/dbpedia.org/viaf.org/scripts/makeLinks.sh
@@ -1,2 +1,2 @@
-wget http://viaf.org/viaf/data/viaf-20160502-clusters-rdf.nt.gz
+wget http://viaf.org/viaf/data/viaf-20161004-clusters-rdf.nt.gz
 zcat viaf-20160502-clusters-rdf.nt.gz | grep sameAs | grep dbpedia | awk -F " " '{print $3 " " $2 " " $1 " ."}' | sort -u > viaf2dbpedia.nt
