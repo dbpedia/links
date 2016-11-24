dc66f82ffdb8a17f5beeb6be9ed1f511395ef1b8
R.P.O.D <rpod@localhost.localdomain>
2016-11-24 15:49:08 +0100

    Updated so it creates backlinks for all linksets via args.

--- tools/scripts/backlinks.py
+++ tools/scripts/backlinks.py
@@ -1,6 +1,9 @@
 from rdflib import Graph, OWL
-from os import getcwd
+from os import getcwd, path
 from pathlib import Path
+import argparse
+
+
 
 class BackLinks:
 
@@ -41,9 +44,55 @@ class BackLinks:
                         backlinks.add( (o1, OWL.sameAs, o) )
         backlinks.serialize(destination=str(self.mainDir) + '/backlinks.nt', format='nt')
 
+    def matchAllBacklinks(self, maingraphs, graphs, target):
+        backlinks = Graph()
+        for maingraph in maingraphs[1]:
+            for s,p,o in maingraph.triples( (None, OWL.sameAs, None) ):
+                for graph in graphs:
+                    for g in graph[1]:
+                        for s1,p1,o1 in g.triples( (s, p, None) ):
+                            backlinks.add( (o1, OWL.sameAs, o) )
+        print(maingraphs[0], path.dirname(maingraphs[0]))
+        backlinks.serialize(destination=target + path.dirname(maingraphs[0]) + '_backlinks.nt', format='nt')
+
+    def allBacklinks(self, target):
+        graphs = []
+        origin = Path(str(self.mainDir) + '/links')
+
+        #readDirs part
+        # dbpedia.org part
+        for dir in Path(str(origin) + '/dbpedia.org').iterdir():
+            tmpGraphs = []
+            for linkset in sorted(dir.glob('*.nt')):
+                tmpGraphs.append(Graph().parse(str(linkset), format='nt'))
+            graphs.append([dir, tmpGraphs[:]])
+
+        # xxx.dbpedia.org part
+        for xdir in Path(str(origin) + '/xxx.dbpedia.org').iterdir():
+            for subdir in xdir.iterdir():
+                tmpGraphs = []
+                for linkset in sorted(subdir.glob('*.nt')):
+                    tmpGraphs.append(Graph().parse(str(linkset), format='nt'))
+                graphs.append([subdir, tmpGraphs[:]])
+
+        #generate Backlinks
+        print(graphs)
+        for i, maingraphs in enumerate(graphs):
+            self.matchAllBacklinks(maingraphs, graphs[:i] + graphs[i+1:], target)
+
+
+
 def main():
-    backlinks = BackLinks(input('Insert full path to linkset directory: '))
-    backlinks.matchBacklinks(backlinks.parseAllGraphs(backlinks.readDirs()))
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dir", help="Set to repo dir, if set create backlinks.nt for all linksets.")
+    parser.add_argument("target", help="Set to target for backlinks, needs to be set when dir is set")
+    args = parser.parse_args()
+    if args.dir and args.target:
+        backlinks = BackLinks(args.dir)
+        backlinks.allBacklinks(args.target)
+    else:
+        backlinks = BackLinks(input('Insert full path to linkset directory: '))
+        backlinks.matchBacklinks(backlinks.parseAllGraphs(backlinks.readDirs()))
 
 if __name__ == '__main__':
     main()
