862a392c086bb0c0eeffeb497e41c6b9adaaf2f7
R.P.O.D <rpod@localhost.localdomain>
2016-11-24 22:04:43 +0100

    Changed table.html title

--- tools/scripts/backlinks.py
+++ tools/scripts/backlinks.py
@@ -66,7 +66,7 @@ class BackLinks:
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Starter Template for Bootstrap</title>
+    <title>Table of backlinks</title>
 
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css" integrity="sha384-AysaV+vQoT3kOAXZkl02PThvDr8HYKPZhNT5h/CXfBThSRXQ6jW5DO2ekP5ViFdi" crossorigin="anonymous">
