5471f41bff6c6fb10deff6ace3349b98ede60b4d
akirsche <amit@informatik.uni-leipzig.de>
2017-07-11 17:13:30 +0200

    Point generate.sh script to the correct file
    The script executed ValidateRepo instead of GenerateLink

--- tools/backend/bin/generate.sh
+++ tools/backend/bin/generate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-mvn install exec:java -q -Dexec.mainClass="org.dbpedia.links.ValidateRepo" -Dexec.args="$*"
+mvn install exec:java -q -Dexec.mainClass="org.dbpedia.links.GenerateLinks" -Dexec.args="$*"
