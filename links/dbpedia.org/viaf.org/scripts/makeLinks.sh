b0ad8579d48cb9f625131e8ff228ea50da8b5be7
Sebastian Hellmann <hellmann@informatik.uni-leipzig.de>
2017-08-09 17:15:15 +0200

    Update makeLinks.sh
    fixed directory

--- links/dbpedia.org/viaf.org/scripts/makeLinks.sh
+++ links/dbpedia.org/viaf.org/scripts/makeLinks.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 LINK=`curl http://viaf.org/viaf/data/ | grep -o 'http://viaf.org/viaf/data/viaf-........-clusters-rdf.nt.gz' | sort -u`
 curl "$LINK" > viaf.nt.gz
-zcat viaf.nt.gz | grep sameAs | grep dbpedia | awk -F " " '{print $3 " " $2 " " $1 " ."}' | sort -u > viaf2dbpedia.nt
+zcat viaf.nt.gz | grep sameAs | grep dbpedia | awk -F " " '{print $3 " " $2 " " $1 " ."}' | sort -u > ../viaf2dbpedia.nt
