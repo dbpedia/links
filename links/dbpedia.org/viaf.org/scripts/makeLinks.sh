00c6a18e1c1ba13556a06bb5cc562d75b48d35e7
Sebastian Hellmann <hellmann@informatik.uni-leipzig.de>
2017-08-09 17:13:28 +0200

    Update makeLinks.sh
    automatic update hack

--- links/dbpedia.org/viaf.org/scripts/makeLinks.sh
+++ links/dbpedia.org/viaf.org/scripts/makeLinks.sh
@@ -1,2 +1,4 @@
-wget http://viaf.org/viaf/data/viaf-20161004-clusters-rdf.nt.gz
-zcat viaf-20160502-clusters-rdf.nt.gz | grep sameAs | grep dbpedia | awk -F " " '{print $3 " " $2 " " $1 " ."}' | sort -u > viaf2dbpedia.nt
+#!/bin/bash
+LINK=`curl http://viaf.org/viaf/data/ | grep -o 'http://viaf.org/viaf/data/viaf-........-clusters-rdf.nt.gz' | sort -u`
+curl "$LINK" > viaf.nt.gz
+zcat viaf.nt.gz | grep sameAs | grep dbpedia | awk -F " " '{print $3 " " $2 " " $1 " ."}' | sort -u > viaf2dbpedia.nt
