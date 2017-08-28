5d2b2b42b79b4668f0efeb2f1aef678bd1b571ff
akirsche <amit@informatik.uni-leipzig.de>
2017-08-28 12:13:46 +0200

    modifications in makeRelease and makeSnapshot

--- makeRelease.sh
+++ makeRelease.sh
@@ -3,19 +3,20 @@
 RELEASEFOLDER=$1
 dbpedia_links=${2:-.} #</path/to/dbpedia-links>
 www=${3:-../www} #</path/to/www>
+REVISIONFOLDER=${4:-$RELEASEFOLDER}
 
 cd $dbpedia_links
 
 # dbpedia.org
-mkdir -p archive/$RELEASEFOLDER/dbpedia.org
-find ./snapshot/dbpedia.org -name "*links.nt.bz2" -exec cp {} archive/$RELEASEFOLDER/dbpedia.org \;
+#mkdir -p archive/$RELEASEFOLDER/dbpedia.org
+#find ./snapshot/dbpedia.org -name "*links.nt.bz2" -exec cp {} archive/$RELEASEFOLDER/dbpedia.org \;
 
 # xxx.dbpedia.org
-LANGUAGES="ja it de nl"
-for i in $LANGUAGES ;do
-mkdir -p archive/$RELEASEFOLDER/xxx.dbpedia.org/$i
-find ./snapshot/xxx.dbpedia.org/$i -name "*links.nt.bz2" -exec cp {} archive/$RELEASEFOLDER/xxx.dbpedia.org/$i \;
-done
+#LANGUAGES="ja it de nl"
+#for i in $LANGUAGES ;do
+#mkdir -p archive/$RELEASEFOLDER/xxx.dbpedia.org/$i
+#find ./snapshot/xxx.dbpedia.org/$i -name "*links.nt.bz2" -exec cp {} archive/$RELEASEFOLDER/xxx.dbpedia.org/$i \;
+#done
 
 chmod 775  $dbpedia_links/archive/$RELEASEFOLDER
-ln -sfn $dbpedia_links/archive/$RELEASEFOLDER $www/downloads.dbpedia.org/links/$RELEASEFOLDER
+ln -sfn $dbpedia_links/archive/$RELEASEFOLDER $www/downloads.dbpedia.org/links/$REVISIONFOLDER
