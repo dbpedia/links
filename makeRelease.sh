29e7387c8681d2739d30c0f899f17da82c059d20
akirsche <amit@informatik.uni-leipzig.de>
2017-08-31 14:37:47 +0200

    fix symlink in makeSnapshot

--- makeRelease.sh
+++ makeRelease.sh
@@ -1,22 +1,16 @@
 #!/bin/sh
 
-RELEASEFOLDER=$1
+SNAPSHOTFOLDER=${1:-snapshot}
 dbpedia_links=${2:-.} #</path/to/dbpedia-links>
 www=${3:-../www} #</path/to/www>
-REVISIONFOLDER=${4:-$RELEASEFOLDER}
+
+DATE=`date +%Y-%m-%d`
+RELEASEFOLDER=${4:-$DATE}
 
 cd $dbpedia_links
 
-# dbpedia.org
-#mkdir -p archive/$RELEASEFOLDER/dbpedia.org
-#find ./snapshot/dbpedia.org -name "*links.nt.bz2" -exec cp {} archive/$RELEASEFOLDER/dbpedia.org \;
-
-# xxx.dbpedia.org
-#LANGUAGES="ja it de nl"
-#for i in $LANGUAGES ;do
-#mkdir -p archive/$RELEASEFOLDER/xxx.dbpedia.org/$i
-#find ./snapshot/xxx.dbpedia.org/$i -name "*links.nt.bz2" -exec cp {} archive/$RELEASEFOLDER/xxx.dbpedia.org/$i \;
-#done
+rm $www/downloads.dbpedia.org/links/$SNAPSHOTFOLDER #remove snapshot link
+mv archive/{$SNAPSHOTFOLDER,$RELEASEFOLDER}
 
 chmod 775  $dbpedia_links/archive/$RELEASEFOLDER
-ln -sfn $dbpedia_links/archive/$RELEASEFOLDER $www/downloads.dbpedia.org/links/$REVISIONFOLDER
+ln -sfn $dbpedia_links/archive/$RELEASEFOLDER $www/downloads.dbpedia.org/links/$RELEASEFOLDER
