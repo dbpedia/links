e3a2b8ffff225be2d91d0d916be01e3045af2a49
akirsche <amit@informatik.uni-leipzig.de>
2018-01-25 15:44:54 +0100

    Fix in makeLinks.sh

--- links/dbpedia.org/yago/scripts/makeLinks.sh
+++ links/dbpedia.org/yago/scripts/makeLinks.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-
 LINK=`curl https://www.mpi-inf.mpg.de/departments/databases-and-information-systems/research/yago-naga/yago/downloads/ | egrep -o "http://resources.mpi-inf.mpg.de/yago-naga/yago([0-9]{1,}\.)+[0-9]{1,}/yagoDBpediaInstances.ttl.7z"`
+curl -O  "$LINK"
+
+#DEBUG
+FILESIZE7Z=$(stat -c%s ./yagoDBpediaInstances.ttl.7z)
+echo "file size of yagoDBpediaInstances.ttl.7z = $FILESIZE7Z b" 
+#
  
-curl -o yagoDBpediaInstances.ttl.7z "$LINK"
- 
-7za e -so -bd yagoDBpediaInstances.ttl.7z 2>/dev/null | grep sameAs | awk -F " " '{ gsub( /\\\\u/,"\\u",$3)  sub(/\</,"<http://yago-knowledge.org/resource/",$1); print $3 " <http://www.w3.org/2002/07/owl#sameAs> " $1 " ."}' | sort -u >  yago_links_ascii.nt
-native2ascii -reverse -encoding UTF-8 yago_links_ascii.nt $1
+7za e -so -bd yagoDBpediaInstances.ttl.7z 2>/dev/null | grep sameAs | awk -F " " '{  gsub( /\\\\/,"\\",$3) gsub( /\\u00/,"%",$3)  sub(/</,"<http://yago-knowledge.org/resource/",$1) gsub(/\\u00/,"%",$1); print $3 " <http://www.w3.org/2002/07/owl#sameAs> " $1 " ."}' | grep -v '\\' |sort -u >  "$1" 
+
+#DEBUG
+FILESIZE_ASCII=$(stat -c%s "$1")
+echo "file size of $1 = $FILESIZE_ASCII b"
+#
 rm yagoDBpediaInstances.ttl.7z
-rm yago_links_ascii.nt
 
 
